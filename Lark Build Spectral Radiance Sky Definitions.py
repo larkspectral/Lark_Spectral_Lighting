1fc1a0c03de8069ac46aa18abad201aed0602e3a
Clotilde Pierson <clotilde.pierson@gmail.com>
2022-08-09 18:45:09 +0200

    Update existing components to Lark 2.0

--- Lark Build Spectral Radiance Sky Definitions.py
+++ Lark Build Spectral Radiance Sky Definitions.py
@@ -1,49 +1,57 @@
 ﻿# Define 3 or 9 channel Radiance skies 
-# Lark Spectral Lighting is a collaboration of University of Washington and ZGF Architects LLP
+
+# Lark Spectral Lighting (v0.0.1, v0.0.2 and v1.0) is a collaboration of University of Washington and ZGF Architects LLP
 # Authors Dr. Mehlika Inanici, Marty Brennan & Ed Clark
+# Lark v2.0 is a collaboration of EPFL, Oregon State University, and Eindhoven University of Technology
+# Authors Dr. Clotilde Pierson & Myrta Gkaintatzi-Masouti
 # Copyright 2015-2022 Mehlika Inanici, Ph.D. (University of Washington) and ZGF Architects LLP
-# Licensed under The 3-Clause BSD License (the "License");
+# Copyright 2022 Clotilde Pierson, Ph.D. (EPFL, Oregon State University) and Myrta Gkaintatzi-Masouti, M.Sc. (Eindhoven University of Technology)
+# Licensed under The Modified 3-Clause BSD License (the "License");
 # You may obtain a copy of the License at
 # https://opensource.org/licenses/BSD-3-Clause
+
+# Component updated by Dr. Priji Balakrishnan & Dr. Alstan J. Jakubiec (2020-05-17): 
+# Updated Radiance sky definition with gendaylit replacing gensky
+
+# Component updated by Dr. Clotilde Pierson for Lark v2.0 (2022-05-10):
+# Removed gen_reindl program since models (Reindl, Erbs…) using only global horizontal sky irradiance for splitting up into direct and diffuse horizontal sky irradiance can show high deviations.
+# Updated Radiance sky definition: gendaylit (version of 2021 in Radiance 5.4) including dew point temperature, RGB components of ground_glow have been set to a grey color (1,1,1) instead of a red color (1,0.8,0.5)
+# Updated inputs (removed sky_type as information not needed for Perez sky model; modified horizontal_direct to normal_direct since gendaylit -W requires normal direct; removed global_illuminance/global_irradiation since gen_reindl program not used anymore; removed run_reindl since gen_reindl program not used anymore; added dew point temperature for more accurate sky definition)
+# Updated outputs (removed reindl_output, diffuse_horizontal and direct_normal since gen_reindl program not used anymore)
+# Updated RGB coefficients: R coefficient updated to 0.2685 (instead of 0.2686) for sum of 3 coefficients to equal 1
 
 """
 Use this component to build Radiance sky definitions for rendering.  
 -
-Provided by lark v1.0
+Provided by Lark 2.0.0
 
     Args:
         channel_output: use Lark SPD component to write 3 or 9 spectral channels 
         channel_type: Choose [0 = 3 channel]  [1 = 9 channel]
         latitude: north of the equator is positive  
         longitude: west of the Prime Meridian is positive
-        UTC:Coordinated Universal Time 
-        sky_type:[0 = clear sunny] [2 = intermediate] [4 = overcast]
+        UTC:Coordinated Universal Time
         month: integer 1-12
         day: integer 1-31
-        hour:float 1-24
-        horizontal_direct: float radiation from measurement
+        hour:float 1-24 (hour should not account for daylight saving time)
+        normal_direct: normal direct irradiance
         horizontal_diffuse: float radiation from measurement
-        global_illuminance: calculated illuminance from unobstructed point looking up for CIE sky condition
-        run_reindl: boolean to start gen_reindl program - place gen_reindl.exe in c:\lark\materials
+        temperature: dew point temperature
         name: optional name for sky spectrum (example: 10k_CCT)
     Returns:
         current_dir: current working folder 
         sky_name_out: Radiance sky definition name
-        reindl_output: gen_reindl txt output [month,day,hour,direct irradiance,diffuse irradiance]
-        diffuse_horizontal: diffuse component of gen_reindl
-        direct_normal: direct normal component of gen_reindl
         sky_mat_3: filepath for 3 channel sky definition
         sky_mat_9a: filepath for 9a channel sky definition
         sky_mat_9b: filepath for 9b channel sky definition
         sky_mat_9c: filepath for 9c channel sky definition
 """
 
-ghenv.Component.Name = "Lark Build Spectral Radiance Sky Definitions"
+ghenv.Component.Name = "Lark Build Spectral Radiance Sky Definitions v2"
 ghenv.Component.NickName = 'Spectral Sky'
-ghenv.Component.Message = 'v1.0'
-ghenv.Component.Category = "Extra"
-ghenv.Component.SubCategory = "lark"
-
+ghenv.Component.Message = '2.0.0'
+ghenv.Component.Category = "Lark"
+ghenv.Component.SubCategory = "Basic Lark"
 
 import re
 import os
@@ -60,16 +68,16 @@ if os.path.isdir(_path) != True:
     os.makedirs(_path)
 else:
     pass
-
 os.chdir(_path)
 #print out current directory
 current_dir = _path
 
 
+
 #check all inputs
 error_list = []
-inputs = [channel_output,channel_type,latitude,longitude,UTC,sky_type,month,day,hour,global_illuminance,run_reindl]
-inputs_name = ["channel_output","channel_type","latitude","longitude","UTC","sky_type","month","day","hour","global_illuminance","run_reindl"]
+inputs = [channel_output,channel_type,latitude,longitude,UTC,month,day,hour,normal_direct,horizontal_diffuse,temperature]
+inputs_name = ["channel_output","channel_type","latitude","longitude","UTC","month","day","hour","normal_direct","horizontal_diffuse","temperature"]
 
 for idx, val in enumerate (inputs):
     if val == None or val == []:
@@ -84,67 +92,23 @@ if check == "error":
     print "Warning! Connect the following inputs:" 
     print error_list
     print check
-#    print len(error_list)
 
 
+
+#assign sky name and process inputs
 if name == None:        
     name = ""
 
 if  check != "error":
-    if sky_type == 0:
-        sky_type = "+s"  #sunny
-        condition_name = "_s"
-    if sky_type == 2:
-        sky_type = "+i"  #intermediate
-        condition_name = "_i"
-    if sky_type == 4:
-        sky_type = "-c"  #overcast
-        condition_name = "_oc"
     space = "_"
-    sky_name = "%s%s%s%s%s%s%s" %(month,space,day,space,hour,condition_name,name)
+    sky_name = "%s%s%s%s%s%s" %(month,space,day,space,hour,name)
     sky_name_out = sky_name
-    #convert global_illuminance to global_irradiance
-    irrad = global_illuminance/179
-
-
-    #write out a gen_reindl tab delimited file
-    reindl_out = str(month) + "\t" + str(day)+ "\t" + str(hour)+ "\t" + str(irrad)   
-
-    irradiance_out = open("reindl_out.txt", "w")
-    irradiance_out.write(reindl_out) 
-    irradiance_out.close()
 
     #convert to minutes
     UTC = int(UTC * -15) #positive west of primer meridian
-
     longitude = longitude * -1  #positive west of prime meridian
 
-    batchfile = "%s %s %s %s %s %s %s" %("gen_reindl -m",UTC,"-l",longitude,"-a",latitude,"-i reindl_out.txt -o reindl.wea")
 
-
-    batch = open("gen_reindl.bat", "w")
-    batch.write(batchfile)
-    batch.close()
-
-
-if run_reindl != True or check == "error":
-    pass
-else:
-    runbatch = Popen("gen_reindl.bat", cwd=_path)
-    stdout, stderr = runbatch.communicate()
-
-#read direct and diffuse outputs from gen_reindl output
-
-    reindl_list = [] 
-    reindl_reader = open("reindl.wea", "r") 
-    for line in reindl_reader:
-        direct,diffuse = line.split()[3:5]
-        reindl_list.append(line)
-    reindl_reader.close()
-
-    reindl_output = reindl_list
-    diffuse_horizontal = diffuse
-    direct_normal  = direct
 
 #photopic coefficients for each channel
 v_b1 = .0004
@@ -152,83 +116,79 @@ v_b2 = .0095
 v_b3 = .0522
 v_g1 = .1288
 v_g2 = .2231
-v_g3 = .3174
+v_g3 = .3175
 v_r1 = .2521
 v_r2 = .0162
 v_r3 = .0002
 
-v_r =  .2686  #.275
-v_g =  .6693  #.670
-v_b =  .0621  #.065  
+v_r =  .2685
+v_g =  .6694
+v_b =  .0621
 
 
 
-if  check != "error" and run_reindl != False:
-    
-    if horizontal_direct == None:
-        direct = round(float(irrad) - float(diffuse),2)
-    else:
-        direct = horizontal_direct
+#prepare variables for sky definition
+if  check != "error":
+    direct = normal_direct
 
     if channel_type == 0:
         if len(channel_output) == 3:
             r,g,b = channel_output #assign channels
             sum3 = r*v_r + g*v_g + b*v_b  #multiply each channel by photopic coefficient (sum is less than 1.0)
-            if horizontal_diffuse == None:
-                diffuse = round(float(diffuse)/ sum3,2)
-            else:
-                diffuse = round(horizontal_diffuse/ sum3,2)
+            #the weighting of the sky model to account for the spectral calculation is done on the RGB components of the sky below so that it does not impact the result of the luminous efficacy model in gendaylit if done on the irradiance
+            diffuse = horizontal_diffuse
         else:
             print "channel_output should equal 3 values"
 
     if channel_type == 1:
         if len(channel_output) == 9:
             b1,b2,b3,g1,g2,g3,r1,r2,r3 = channel_output  #assign channels
-            sum9 = b1*v_b1 + b2*v_b2 + b3*v_b3 + g1*v_g1 + g2*v_g2 + g3*v_g3 + r1*v_r1 + r2*v_r2 + r3*v_r3  #multiply each channel by photopic coefficient (sum is less than 1.0)
-            if horizontal_diffuse == None:
-                diffuse = round(float(diffuse)/ sum9,2)
-            else:
-                diffuse = round(horizontal_diffuse/ sum9,2)
+            sum9 = b1*v_b1 + b2*v_b2 + b3*v_b3 + g1*v_g1 + g2*v_g2 + g3*v_g3 + r1*v_r1 + r2*v_r2 + r3*v_r3 #multiply each channel by photopic coefficient (sum is less than 1.0)
+            #the weighting of the sky model to account for the spectral calculation is done on the RGB components of the sky below so that it does not impact the result of the luminous efficacy model in gendaylit if done on the irradiance
+            diffuse = horizontal_diffuse
         else:
             print "channel_output should equal 9 values"
 
 
 
-    gensky =  "%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s \n" %("!gensky",month,day,hour,sky_type,"-B",diffuse,"-R",direct,"-a",latitude,"-o",longitude,"-m",UTC)+ "\n" 
+#write Radiance sky definition
+    gendaylit =  "%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s \n" %("!gendaylit",month,day,hour,"-W",direct,diffuse,"-d",temperature,"-a",latitude,"-o",longitude,"-m",UTC)+ "\n"
     sky = "%s %s %s \n" %("sky_mat","source","sky") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(0,0,1,180) + "\n"*2
-    ground_glow = "%s %s %s \n" %("skyfunc","glow","ground_glow") + ("0" + "\n")*2 + ("4" + "\n")+ "%s %s %s %s" %(1,0.8,0.5,0)+ "\n"*2 
+    ground_glow = "%s %s %s \n" %("skyfunc","glow","ground_glow") + ("0" + "\n")*2 + ("4" + "\n")+ "%s %s %s %s" %(1,1,1,0)+ "\n"*2
     ground = "%s %s %s \n" %("ground_glow","source","ground") + ("0" + "\n")*2 +("4" + "\n") +"%s %s %s %s" %(0,0,-1,180)
     sky_def = sky + ground_glow + ground + "\n" * 2
 
 
+
+#write Radiance sky file
     if channel_type == 0:
-        sky_mat =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 +("4" + "\n") + "%s %s %s %s" %(r,g,b,0) + "\n" * 2
+        #the weighting of the sky model to account for the spectral calculation is done here on the RGB components of the sky so that it does not impact the result of the luminous efficacy model in gendaylit if done on the irradiance
+        sky_mat =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 +("4" + "\n") + "%s %s %s %s" %(round(r/sum3,2),round(g/sum3,2),round(b/sum3,2),0) + "\n" * 2
         material = open(sky_name+".rad", "w")
-        material.write(gensky + sky_mat + sky_def)
+        material.write(gendaylit + sky_mat + sky_def)
         material.close()
 
-        
     if channel_type == 1:
-        sky_mat_a =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(b1,b2,b3,0) + "\n" * 2
-        sky_mat_b =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(g1,g2,g3,0) + "\n" * 2
-        sky_mat_c =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(r1,r2,r3,0) + "\n" * 2
+        #the weighting of the sky model to account for the spectral calculation is done here on the RGB components of the sky so that it does not impact the result of the luminous efficacy model in gendaylit if done on the irradiance
+        sky_mat_a =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(round(b1/sum9,2),round(b2/sum9,2),round(b3/sum9,2),0) + "\n" * 2
+        sky_mat_b =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(round(g1/sum9,2),round(g2/sum9,2),round(g3/sum9,2),0) + "\n" * 2
+        sky_mat_c =  "%s %s %s \n" %("skyfunc","glow","sky_mat") + ("0" + "\n")*2 + ("4" + "\n") + "%s %s %s %s" %(round(r1/sum9,2),round(r2/sum9,2),round(r3/sum9,2),0) + "\n" * 2
 
         materiala = open(sky_name + "_a" +".rad", "w")
-        materiala.write(gensky + sky_mat_a + sky_def)
+        materiala.write(gendaylit + sky_mat_a + sky_def)
         materiala.close()    
 
         materialb = open(sky_name + "_b" +".rad", "w")
-        materialb.write(gensky + sky_mat_b + sky_def)
+        materialb.write(gendaylit + sky_mat_b + sky_def)
         materialb.close() 
 
         materialc = open(sky_name + "_c" +".rad", "w")
-        materialc.write(gensky + sky_mat_c + sky_def)
+        materialc.write(gendaylit + sky_mat_c + sky_def)
         materialc.close() 
 
 
 
-
-    #print filepaths for output pictures (all pictures are greyscale luminance to be viewed in falsecolor)
+#print filepaths for rad sky
     sky_mat_3 = os.getcwd() + "\\" + sky_name+".rad"
     sky_mat_9a = os.getcwd() + "\\" + sky_name + "_a" +".rad"
     sky_mat_9b = os.getcwd() + "\\" + sky_name + "_b" +".rad"
--- Lark Convert SPD - Write Spectral Radiance Materials.py
+++ Lark Convert SPD - Write Spectral Radiance Materials.py
@@ -1,15 +1,22 @@
 ﻿# Convert SPD to 3 or 9 channel Radiance materials
-# Lark Spectral Lighting is a collaboration of University of Washington and ZGF Architects LLP
+
+# Lark Spectral Lighting (v0.0.1, v0.0.2 and v1.0) is a collaboration of University of Washington and ZGF Architects LLP
 # Authors Dr. Mehlika Inanici, Marty Brennan & Ed Clark
+# Lark v2.0 is a collaboration of EPFL, Oregon State University, and Eindhoven University of Technology
+# Authors Dr. Clotilde Pierson & Myrta Gkaintatzi-Masouti
 # Copyright 2015-2022 Mehlika Inanici, Ph.D. (University of Washington) and ZGF Architects LLP
-# Licensed under The 3-Clause BSD License (the "License");
+# Copyright 2022 Clotilde Pierson, Ph.D. (EPFL, Oregon State University) and Myrta Gkaintatzi-Masouti, M.Sc. (Eindhoven University of Technology)
+# Licensed under The Modified 3-Clause BSD License (the "License");
 # You may obtain a copy of the License at
 # https://opensource.org/licenses/BSD-3-Clause
+
+# Component updated by Dr. Clotilde Pierson for Lark v2.0 (2022-05-10):
+# Added a fourth material type ([3 = electriclight]) and defined the main path accordingly
 
 """
 Use this component to convert spectral data to Radiance materials
 -
-Provided by lark v1.0
+Provided by Lark 2.0.0
 
     Args:
         spd_input: filepath to spectral power distribution txt file. column 1 = wavelength/ column 2 = transmittance or reflectance
@@ -18,7 +25,7 @@ Provided by lark v1.0
         material_name: Name of Radiance material (*no white space)
         roughness: float  0-1
         specularity: float 0-1
-        material_type: [0 = glass] [1 = plastic] [2 = sky] 
+        material_type: [0 = glass] [1 = plastic] [2 = sky] [3 = electriclight]
         channel_type:  [0 = 3 channel]  [1 = 9 channel]  3 channel is RGB/ 9 channel is b1,b2,b3,g1,g2,g3,r1,r2,r3
     Returns:
         current_dir: current working folder 
@@ -29,12 +36,11 @@ Provided by lark v1.0
         mat_9c: filepath to 9 channel Radiance material definition [r1,r2,r3]
 """
 
-ghenv.Component.Name = "Lark Convert SPD - Write Spectral Radiance Materials"
+ghenv.Component.Name = "Lark Convert SPD - Write Spectral Radiance Materials v2"
 ghenv.Component.NickName = 'Spectral Materials'
-ghenv.Component.Message = 'v1.0'
-ghenv.Component.Category = "Extra"
-ghenv.Component.SubCategory = "lark"
-
+ghenv.Component.Message = '2.0.0'
+ghenv.Component.Category = "Lark"
+ghenv.Component.SubCategory = "Basic Lark"
 
 import re
 import os
@@ -43,21 +49,9 @@ import math
 from collections import OrderedDict
 from decimal import Decimal
 
-#make lark directory if none exists
-_path = 'C:\lark\materials'
-if os.path.isdir(_path) != True: 
-    os.makedirs(_path)
-else:
-    pass
-
-os.chdir(_path)
-#print out current directory
-current_dir = _path
 
 
-
-#print "component will read an optics6 spectral data grid export or a 2-column text spectral text file. Be sure to input the correct source_interval. example 1, 2 or 10 nanometer intervals"
-
+#check for missing inputs
 error_list = []
 inputs = [spd_input,source_interval,material_name,material_type,channel_type]
 inputs_name = ["spd_input","source_interval","material_name","material_type","channel_type"]
@@ -76,11 +70,22 @@ else:
 if check == "error" :
     print "Warning! Connect the following inputs:" 
     print error_list
-#    print len(error_list)
 
 
 
-
+#create and assign working directory
+if material_type == 3 : #main folder for electric light
+    _path = 'C:\lark\luminaires\ies'
+else: #main folder in other cases
+    _path = 'C:\lark\materials'
+#make lark directory if none exists
+if os.path.isdir(_path) != True: 
+    os.makedirs(_path)
+else:
+    pass
+os.chdir(_path)
+#print out current directory
+current_dir = _path
 
 
 
@@ -88,6 +93,7 @@ if check == "error" :
 #read lines of spectral data from 2 column text file - first column must be wavelength; the second is for values
 #Note, values should be between 0 to 1. excel_daylight_series_cal data will be converted automatically
 if  check != "error":
+    print spd_input
     fopen = open(spd_input, 'r')
     b = fopen.readlines()
     data = [] #create list
@@ -102,15 +108,11 @@ if  check != "error":
         (wave, val) =  line[0], line[1] #assign first 2 columns of data
         spd[float(wave)] = float(val)  #assign values to dictionary keys
 
-        
-    
     for keyA in spd.keys():
         if keyA < 10:  #true for Optics6 export which measures wavelenth in microns 
             spd[keyA*1000] = spd.pop(keyA)  #replace old dict key with new key
         else:
             pass
-
-#    print spd[380]
 
     #If excel_daylight_series_cal data is used, values need to be normalized 0 to 1
     maxkey = max(spd, key=lambda i: spd[i])
@@ -125,11 +127,14 @@ if  check != "error":
             pass
 
 
+
 #interpolate function
 def interpolate(value1,value2,key1,key2,increment): 
     slope = (value2-value1)/(key2-key1)
     value = value1 + (slope*increment)
     return float(value)
+
+
 
 #run interpolate function on spectral power distribution
 def run_interpolate (dict, bin, interval):   # nanometer bins from source file, interval to interpolate
@@ -141,11 +146,9 @@ if  check != "error" :
     for iteration in xrange (1,source_interval): # iterate run_interpolate function at 1 nm intervals over source_interval
         run_interpolate (spd,source_interval,iteration)
 
-#print spd[382]
 
-#string value = dictionary.ContainsKey(key) ? dictionary[key] : "default";
 
-#  bin and channel are interchangeable terms: bins between 380 - 780nm for 9 and 3 channels 
+#bin and channel are interchangeable terms: bins between 380 - 780nm for 9 and 3 channels 
 bin9 = [380,422],[422,460],[460,498],[498,524],[524,550],[550,586],[586,650],[650,714],[714,780] #9 channels: b1,b2,b3,g1,g2,g3,r1,r2,r3 
 bin3 = [586,780],[498,586],[380,498]  #3 channels: r,g,b 
 
@@ -153,6 +156,7 @@ if channel_type == 0:
     binType = bin3
 if channel_type == 1:
     binType = bin9
+
 
 
 #avg_count iterates within wavelength bin to return average
@@ -168,9 +172,8 @@ def avg_count(dict, channel):  #9 channels to choose from
 
 
 
-avg_channel = []   #output average color per channel
-
 #use avg_count function on each bin to generate a list of averages
+avg_channel = []   #output average color per channel
 def avg_list(dict):
     for binX in xrange (len(binType)): #iterates to 3 or 9
         avg_channel.append(avg_count(dict,binX)) #iterate avg_count over all channels
@@ -183,10 +186,12 @@ if check != "error":
         print "warning: channel data should be betwen 0 & 1. Set excel_daylight_series_cal data to true"
 
 
-#Convert transmittance to transmissivity for Radiance material definition
+
+#convert transmittance to transmissivity for Radiance material definition
 def glazing_transmissivity(Tn):
     transmissivity = (math.sqrt(0.8402528435+(0.0072522239*(Tn * Tn)))-0.9166530661)/(0.003626119*Tn)
     return round (transmissivity,3)
+
 
 
 #convert list of average transmittance to transmissivity
@@ -195,36 +200,34 @@ if material_type == 0:   #glass type
         avg_channel[index] = glazing_transmissivity (item)
 
 
+
+#print channel_output
 channel_output = avg_channel
 
 
+
+#assign main material properties
 if material_type == 0:
     type = "glass"
 if material_type == 1:
     type = "plastic"
 
-
 modifier = "void" #overwrite for custom Radiance modifiers
 
 
 
-#assign variables to 3 or 9 channels and write Radiance definition
+#write Radiance material definition
 if  check != "error" and material_type == 0:  #glazing material
 
     if channel_type == 0:  #3-channel
         r,g,b = avg_channel
-        material = open(material_name+".rad", "w")
-        material.write(
-        "%s %s %s \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "3 %s %s %s" %(r,g,b))  
-        material.close()
-    
+        mat3 = "%s %s %s \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "3 %s %s %s" %(r,g,b)
+
     if channel_type == 1: #9-channel
         b1,b2,b3,g1,g2,g3,r1,r2,r3 = avg_channel
-
         mat_a = "%s %s %s_a \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "3 %s %s %s" %(b1,b2,b3) 
         mat_b = "%s %s %s_b \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "3 %s %s %s" %(g1,g2,g3) 
         mat_c = "%s %s %s_c \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "3 %s %s %s" %(r1,r2,r3)
-
 
 if  check != "error" and material_type == 1:  #plastic material
 
@@ -232,7 +235,7 @@ if  check != "error" and material_type == 1:  #plastic material
         roughness = 0
     if roughness > 1:
         print "roughness should be between 0 -1 "
-        
+
     if specularity == None:
         specularity = 0
     if specularity > 1:
@@ -240,21 +243,23 @@ if  check != "error" and material_type == 1:  #plastic material
 
     if channel_type == 0: #3 channel
         r,g,b = avg_channel
-        material = open(material_name+".rad", "w")
-        material.write(
-        "%s %s %s \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "5 %s %s %s %s %s" %(r,g,b,specularity,roughness))  
-        material.close()
-    
+        mat3 = "%s %s %s \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "5 %s %s %s %s %s" %(r,g,b,specularity,roughness)
+
     if channel_type == 1: #9 channel
         b1,b2,b3,g1,g2,g3,r1,r2,r3 = avg_channel
-
         mat_a = "%s %s %s_a \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "5 %s %s %s %s %s" %(b1,b2,b3,specularity,roughness) 
         mat_b = "%s %s %s_b \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "5 %s %s %s %s %s" %(g1,g2,g3,specularity,roughness) 
         mat_c = "%s %s %s_c \n" %(modifier,type,material_name)+ ("0" + "\n")*2 + "5 %s %s %s %s %s" %(r1,r2,r3,specularity,roughness)
 
 
 
-if  check != "error" and channel_type == 1 and material_type != 2:
+#write radiance material files
+if  check != "error" and channel_type == 0 and material_type != 2 and material_type != 3:
+    material = open(material_name + ".rad", "w")
+    material.write(mat3)
+    material.close()
+
+if  check != "error" and channel_type == 1 and material_type != 2 and material_type != 3:
     materiala = open(material_name + "_a.rad", "w")
     materiala.write(mat_a)
     materiala.close()    
@@ -268,11 +273,12 @@ if  check != "error" and channel_type == 1 and material_type != 2:
     materialc.close()
 
 
-#print filepaths for output pictures 
-if  check != "error" and channel_type == 0 and material_type != 2:
+
+#print filepaths for rad materials
+if  check != "error" and channel_type == 0 and material_type != 2 and material_type != 3:
     mat_3 = os.getcwd() + "\\" + material_name+".rad"
     
-if  check != "error" and channel_type == 1 and material_type != 2:
+if  check != "error" and channel_type == 1 and material_type != 2 and material_type != 3:
     mat_9a = os.getcwd() + "\\" + material_name + "_a.rad"
     mat_9b = os.getcwd() + "\\" + material_name + "_b.rad"
     mat_9c = os.getcwd() + "\\" + material_name + "_c.rad"
@@ -280,12 +286,11 @@ else:
     pass
 
 
+
+#write channel_output file
 if  check != "error":
     #output avg_channel
     g = open('spd_avg.txt', 'w')
     for item in avg_channel:
         g.write(str(item) + '\n')
     g.close()
-
-
-
